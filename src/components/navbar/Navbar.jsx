5026da1080764425364bc0b9dbeddee7e0c4f9d3
sava <savamedvedev@gmail.com>
2022-05-03 22:15:30 +0800

    second-commit

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,15 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import "../navbar/NavbarStyles.css";
 import { FaBars, FaTimes } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  const [nav, setNav] = useState(false);
+  const handleClick = () => setNav(!nav);
   return (
     <div className="header">
       <Link to="">
         <h1>GLX TRVL</h1>
       </Link>
-      <ul className="nav-menu">
+      <ul className={nav ? "nav-menu active" : "nav-menu"}>
         <li>
           <Link to="/">Home</Link>
         </li>
@@ -23,8 +25,12 @@ const Navbar = () => {
           <Link to="/contact">Contact</Link>
         </li>
       </ul>
-      <div className="hamburger">
-        <FaBars style={{ color: "#fff" }} />
+      <div className="hamburger" onClick={handleClick}>
+        {!nav ? (
+          <FaBars style={{ color: "#fff" }} className="icon" />
+        ) : (
+          <FaTimes style={{ color: "#fff" }} className="icon" />
+        )}
       </div>
     </div>
   );
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,10 +1,14 @@
 import React from "react";
+import Footer from "../components/footer/Footer";
 import Navbar from "../components/navbar/Navbar";
+import Video from "../components/video/Video";
 
 const Home = () => {
   return (
     <>
       <Navbar />
+      <Video />
+      <Footer />
     </>
   );
 };
