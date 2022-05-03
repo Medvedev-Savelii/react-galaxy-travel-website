54dbcb07cf2a48dd1c7e41fcfcf6e8663c33542d
sava <savamedvedev@gmail.com>
2022-05-04 00:37:03 +0800

    thirdd-commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Home from "./routes/Home";
+import Pricing from "./routes/Pricing";
 import { Routes, Route } from "react-router-dom";
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/pricing" element={<Pricing />} />
       </Routes>
     </>
   );
---
+++ src/routes/Pricing.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Footer from "../components/footer/Footer";
+import Navbar from "../components/navbar/Navbar";
+import HeroImage from "../components/heroimage/HeroImage";
+const Pricing = () => {
+  return (
+    <>
+      <Navbar />
+      <HeroImage heading="PRICING." text="Choose your trip." />
+      <Footer />
+    </>
+  );
+};
+
+export default Pricing;
